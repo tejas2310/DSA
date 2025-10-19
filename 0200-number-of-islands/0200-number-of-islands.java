4607ce2240d4065e86345c1f7e704d51e61152f5
tejas2310 <66642149+tejas2310@users.noreply.github.com>
2025-10-19 17:00:12 +0530

    Time: 8 ms (12.09%), Space: 49.4 MB (80.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -8,19 +8,19 @@ public static int numIslands(char[][] grid) {
         int x;
         int y;
         Queue<int[]> q = new ArrayDeque<>();
-        int[][] isVisited = new int[grid.length][grid[0].length];
+
         for(int i = 0 ; i < grid.length ; i++){
             for(int j = 0; j < grid[0].length ; j++){
-                if(grid[i][j] == '1' && isVisited[i][j] != 1){
-                    isVisited[i][j] = 1;
+                if(grid[i][j] == '1'){
+                    grid[i][j] = '0';
                     q.offer(new int[]{i,j});
                     while(!q.isEmpty()){
                         int[] curr = q.poll();
                         for(int[] side : sides){
                             x = curr[0] + side[0];
                             y = curr[1] + side[1];
-                            if(x >= 0 && x <= grid.length-1 && y >= 0 && y <= grid[0].length-1 && grid[x][y] == '1' && isVisited[x][y] != 1){
-                                isVisited[x][y] = 1;
+                            if(x >= 0 && x <= grid.length-1 && y >= 0 && y <= grid[0].length-1 && grid[x][y] == '1'){
+                                grid[x][y] = '0';
                                 q.offer(new int[]{x,y});
                             }
                         }
