570ecdd7c128f06d75b1c2055826fba5d530a2a6
tejas2310 <66642149+tejas2310@users.noreply.github.com>
2026-01-14 20:01:13 +0530

    Time: 51 ms (81.26%), Space: 65.2 MB (97.25%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,24 +1,21 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
-        int start = 0;
-        int end = 0;
-        
-        int result = Integer.MIN_VALUE;
+        Map<Integer, Integer> freq = new HashMap<>();
+        int start = 0, result = 0;
 
-        while(end<fruits.length){
-            freqMap.merge(fruits[end],1,Integer::sum);
+        for (int end = 0; end < fruits.length; end++) {
+            freq.merge(fruits[end], 1, Integer::sum);
 
-            if(freqMap.size() > 2){
-                result = Math.max(end-start, result);
-                while(freqMap.size() > 2){
-                    freqMap.merge(fruits[start],-1,Integer::sum);
-                    if(freqMap.get(fruits[start]) == 0) freqMap.remove(fruits[start]);
-                    start++;
+            while (freq.size() > 2) {
+                freq.merge(fruits[start], -1, Integer::sum);
+                if (freq.get(fruits[start]) == 0) {
+                    freq.remove(fruits[start]);
                 }
+                start++;
             }
-            end++;
+
+            result = Math.max(result, end - start + 1);
         }
-        return Math.max(end-start, result);
+        return result;
     }
 }
