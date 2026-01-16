b32a2dd70c324f569cbac791c0ecf174793b6ddc
tejas2310 <66642149+tejas2310@users.noreply.github.com>
2026-01-16 18:17:52 +0530

    Time: 1 ms (99.87%), Space: 46.6 MB (99.01%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,54 +1,19 @@
 class Solution {
-    public static int[] sortedSquares(int[] nums) {
-        if(nums[0] >= 0){
-            for(int i = 0; i < nums.length;i++){
-                nums[i] = nums[i] * nums[i];
-            }
-            return nums;
-        }
-        int i = 0;
-        int j = nums.length-1;
-        int mid = 0;
-        int count = 0;
-        int[] result = new int[nums.length];
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
 
-        while(i<=j){
-            mid = (i+j)/2;
-            if(nums[mid] >= 0){
-                if(nums[mid-1]<0){
-                    break;
-                }
-                j = mid-1;
-            }else{
-                i = mid+1;
-            }
-        }
+        int left = 0, right = n - 1;
+        int pos = n - 1;
 
-        i = mid-1;
-        j = mid;
-
-        while(count!=result.length){
-            if(i<0){
-                result[count] = nums[j] * nums[j];
-                j++;
-            }else if(j>result.length-1){
-                result[count] = nums[i] * nums[i];
-                i--;
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[pos--] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[pos--] = nums[right] * nums[right];
+                right--;
             }
-            else if(Math.abs(nums[i]) > Math.abs(nums[j])){
-                result[count] = nums[j] * nums[j];
-                j++;
-            }else if(Math.abs(nums[i]) < Math.abs(nums[j])){
-                result[count] = nums[i] * nums[i];
-                i--;
-            }else{
-                result[count] = nums[j] * nums[j];
-                count++;
-                result[count] = nums[i] * nums[i];
-                i--;
-                j++;
-            }
-            count++;
         }
         return result;
     }
