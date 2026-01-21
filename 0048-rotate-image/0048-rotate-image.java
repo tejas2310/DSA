5bead9163392fee945e578da10dafd8b0f3e5206
tejas2310 <66642149+tejas2310@users.noreply.github.com>
2026-01-21 18:45:47 +0530

    Time: 0 ms (100%), Space: 43.7 MB (59.85%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,25 +1,16 @@
 class Solution {
 public static void rotate(int[][] matrix) {
-        int iterX;
-        int iterY;
-        int iterZ;
-        int curr;
-        int next;
-        for(int i = 0; i<matrix.length;i++){
-            for(int j = i; j<matrix[0].length-1-i;j++){
-                curr = matrix[i][j];
-                iterX = i;
-                iterY = j;
-                do{
-                    iterZ = iterX;
-                    iterX = iterY;
-                    iterY = matrix[0].length-1-iterZ;
-                    next = matrix[iterX][iterY];
-                    matrix[iterX][iterY] = curr;
-                    curr = next;
-                }while(!(iterX == i && iterY == j));
-            }
+    int n = matrix.length;
+    int temp;
+    for(int i =0;i<n;i++){
+        for(int j = i; j<n-i-1;j++){
+            temp = matrix[i][j]; 
+            matrix[i][j] = matrix[n-1-j][i]; 
+            matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+            matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+            matrix[j][n-1-i] = temp;
         }
-        System.out.println(Arrays.deepToString(matrix));
     }
+
+}
 }
